55bc95b0b2a612f22064127db1c882239d81a6dc
Marcela Benachi <mbenachi17@gmail.com>
2024-04-23 17:02:14 -0500

    Subiendo unos cambios chiquitos

---
+++ CrudMercados/src/main/java/com/codeinnova/CrudMercados/Controllers/MarketsController.java
@@ -0,0 +1,8 @@
+package com.codeinnova.CrudMercados.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MarketsController {
+
+}
--- CrudMercados/src/main/java/com/codeinnova/CrudMercados/CrudMercadosApplication.java
+++ CrudMercados/src/main/java/com/codeinnova/CrudMercados/CrudMercadosApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.Set;
+
 @SpringBootApplication
 public class CrudMercadosApplication {
 
@@ -14,7 +19,28 @@ public class CrudMercadosApplication {
 		ApplicationContext context = SpringApplication.run(CrudMercadosApplication.class, args);
 		MarketsRepository repository = context.getBean(MarketsRepository.class);
 
+		Set<DayOfWeek> openingDays = new HashSet<>();
+		openingDays.add(DayOfWeek.MONDAY);
+		openingDays.add(DayOfWeek.TUESDAY);
+		openingDays.add(DayOfWeek.WEDNESDAY);
+		openingDays.add(DayOfWeek.THURSDAY);
+		openingDays.add(DayOfWeek.FRIDAY);
+		openingDays.add(DayOfWeek.SATURDAY);
+		openingDays.add(DayOfWeek.SUNDAY);
+
 		//Create market
+
+		Markets market1 = new Markets(null, "Pedro Market", "Calle 5 #7-19", openingDays,
+				LocalTime.of(7, 0), LocalTime.of(20, 0), true, false,
+				"legumbres,Tuberculos, Frutas, Carnes");
+
+		//DB
+
+		System.out.println("Markets in the database " + repository.findAll().size());
+
+		repository.save(market1);
+
+		System.out.println("Markets in the database " + repository.findAll().size());
 
 	}
 
